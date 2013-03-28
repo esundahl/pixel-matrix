38c50cdfc510ab1c50b712a87afc81fb4d5d1390
Erik Sundahl <esundahl@gmail.com>
2013-03-27 22:41:29 -0700

    basic selecting methods

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 
 
 /**
+ * Dependencies
+ */
+
+var enumerable = require('enumerable');
+
+
+/**
  * Private Variables
  */
 
@@ -20,9 +27,7 @@ function Matrix (x, y, size) {
 	this.matrix = document.createElement('ul');
 	this.matrix.classList.add('matrix');
 	this.matrix.style.width = ((x * size) + (x * 2)) + 'px';
-	console.log(((x * size) + (x * 2)));
 	this.matrix.style.height = ((y * size) + (y * 2)) + 'px';
-	console.log(((y * size) + (y * 2)));
 	this.pixels = [];
 	for (var i = 0; i < (x * y); i++) {
 		var pixel = pixelTmpl.cloneNode(true);
@@ -40,12 +45,47 @@ function Matrix (x, y, size) {
 /**
  * Returns the matrix dom element
  * @return {Element}
- * @api    public
+ * @api public
  */
 
 Matrix.prototype.el = function () {
 	return this.matrix;
 }
+
+
+Matrix.prototype.clear = function () {
+	for (var i = 0; i < this.pixels.length; i++) {
+	  this.pixels[i].style.background = "rgb(255, 255, 255)";
+	}
+}
+
+Matrix.prototype.all = function () {
+	return this.pixels;
+}
+
+Matrix.prototype.at = function (index, y) {
+	index -= 1;
+	if (y) {
+		y -= 1;
+	  return [this.pixels[index + (y * this.y)]];
+	}
+	return [this.pixels[index]];
+}
+
+Matrix.prototype.row = function (index) {
+	return this.pixels.filter(function (pixel, i) {
+		var g = (index - 1) * this.x;
+		return (i >= g && i < (g + this.x));
+	}, this);
+}
+
+Matrix.prototype.column = function (index) {
+	return this.pixels.filter(function (pixel, i) {
+		return ((i + 1) % 8 === index);
+	}, this);
+}
+
+
 
 
 /**
